f7a3e95288215017aeb4f4e086f67dfccc4b782b
Will Richardson <rich1051@umn.edu>
2023-06-20 16:46:52 -0500

    lecture done

--- server/routes/pets.router.js
+++ server/routes/pets.router.js
@@ -1,29 +1,51 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const pool = require('../modules/pool');
+const pool = require("../modules/pool");
 
 // This route *should* return the logged in users pets
-router.get('/', (req, res) => {
-    console.log('/pet GET route');
-    console.log('is authenticated?', req.isAuthenticated());
-    console.log('user', req.user);
+router.get("/", (req, res) => {
+  if (req.isAuthenticated()) {
+    console.log("/pet GET route");
+    console.log("is authenticated?", req.isAuthenticated());
+    console.log("user", req.user);
     let queryText = `SELECT * FROM "pets"`;
-    pool.query(queryText).then((result) => {
+    pool
+      .query(queryText)
+      .then((result) => {
         res.send(result.rows);
-    }).catch((error) => {
+      })
+      .catch((error) => {
         console.log(error);
         res.sendStatus(500);
-    });
+      });
+  } else {
+    res.sendStatus(403);
+  }
 });
 
 // This route *should* add a pet for the logged in user
-router.post('/', (req, res) => {
-    console.log('/pet POST route');
+router.post("/", (req, res) => {
+// if user is logged in AND access level is 0:
+  if (req.isAuthenticated() && req.user.access_level === 0) {
+    console.log("/pet POST route");
     console.log(req.body);
-    console.log('is authenticated?', req.isAuthenticated());
-    console.log('user', req.user);
-    res.sendStatus(200);
-    
+    console.log("is authenticated?", req.isAuthenticated());
+    console.log("user", req.user);
+    //   res.sendStatus(200);
+    let queryText = `INSERT INTO "pets" ("name", "user_id")
+                VALUES ($1, $2);`;
+    pool
+      .query(queryText, [req.body.name, req.user.id])
+      .then((result) => {
+        res.sendStatus(201);
+      })
+      .catch((error) => {
+        console.log("Error:", error);
+        res.sendStatus(500);
+      });
+  } else {
+    res.sendStatus(403);
+  }
 });
 
 module.exports = router;
